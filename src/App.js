9b1b13e7409306b87ab80ae584d6dc7e198d58bf
Petro Bahrynovskyi <pbahrynovskyi@gmail.com>
2024-04-28 23:06:06 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [showFinalResults, setFinalResults] = useState(false);
+  const [score, setsCore] = useState(0);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>USA Quiz</h1>
+
+      <h2>Current Score: 2</h2>
+      {showFinalResults ? (
+        <div className="final-results">
+          <h1>Final Results</h1>
+          <h2>
+            1 out of 5 correct - (20%)
+            <button>Restart Game</button>
+          </h2>
+        </div>
+      ) : (
+        <div className="question-card">
+          <h2>Question 1 out of 5</h2>
+          <h3 className="question-text">What is the capital of the US?</h3>
+
+          <ul>
+            <li>Boston</li>
+            <li>New York City</li>
+            <li>Washington DC</li>
+            <li>Denver</li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
