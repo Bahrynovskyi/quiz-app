22085f774565524b7aeda50360c592d6d2d11bbc
Bahrynovskyi <167538509+Bahrynovskyi@users.noreply.github.com>
2024-04-28 23:45:31 +0300

    add logic, create a few functions

--- src/App.js
+++ src/App.js
@@ -2,33 +2,109 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [showFinalResults, setFinalResults] = useState(false);
-  const [score, setsCore] = useState(0);
+  // Properties
+  const [showResults, setShowResults] = useState(false);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [score, setScore] = useState(0);
 
+  const questions = [
+    {
+      text: "What is the capital of America?",
+      options: [
+        { id: 0, text: "New York City", isCorrect: false },
+        { id: 1, text: "Boston", isCorrect: false },
+        { id: 2, text: "Santa Fe", isCorrect: false },
+        { id: 3, text: "Washington DC", isCorrect: true },
+      ],
+    },
+    {
+      text: "What year was the Constitution of America written?",
+      options: [
+        { id: 0, text: "1787", isCorrect: true },
+        { id: 1, text: "1776", isCorrect: false },
+        { id: 2, text: "1774", isCorrect: false },
+        { id: 3, text: "1826", isCorrect: false },
+      ],
+    },
+    {
+      text: "Who was the second president of the US?",
+      options: [
+        { id: 0, text: "John Adams", isCorrect: true },
+        { id: 1, text: "Paul Revere", isCorrect: false },
+        { id: 2, text: "Thomas Jefferson", isCorrect: false },
+        { id: 3, text: "Benjamin Franklin", isCorrect: false },
+      ],
+    },
+    {
+      text: "What is the largest state in the US?",
+      options: [
+        { id: 0, text: "California", isCorrect: false },
+        { id: 1, text: "Alaska", isCorrect: true },
+        { id: 2, text: "Texas", isCorrect: false },
+        { id: 3, text: "Montana", isCorrect: false },
+      ],
+    },
+    {
+      text: "Which of the following countries DO NOT border the US?",
+      options: [
+        { id: 0, text: "Canada", isCorrect: false },
+        { id: 1, text: "Russia", isCorrect: true },
+        { id: 2, text: "Cuba", isCorrect: true },
+        { id: 3, text: "Mexico", isCorrect: false },
+      ],
+    },
+  ];
+
+
+  const optionClicked = (isCorrect) => {
+    if (isCorrect) {
+      setScore(score + 1);
+    }
+
+    if (currentQuestion + 1 < questions.length) {
+      setCurrentQuestion(currentQuestion + 1);
+    } else {
+      setShowResults(true);
+    }
+  };
+  const restartGame = () => {
+    setScore(0);
+    setCurrentQuestion(0);
+    setShowResults(false);
+  };
 
   return (
     <div className="App">
-      <h1>USA Quiz</h1>
-
-      <h2>Current Score: 2</h2>
-      {showFinalResults ? (
+      <h1>USA Quiz 🇺🇸</h1>
+      <h2>Score: {score}</h2>
+      {showResults ? (
+        /* 4. Final Results */
         <div className="final-results">
           <h1>Final Results</h1>
           <h2>
-            1 out of 5 correct - (20%)
-            <button>Restart Game</button>
+            {score} out of {questions.length} correct - (
+            {(score / questions.length) * 100}%)
           </h2>
+          <button onClick={() => restartGame()}>Restart game</button>
         </div>
       ) : (
-        <div className="question-card">
-          <h2>Question 1 out of 5</h2>
-          <h3 className="question-text">What is the capital of the US?</h3>
 
+        <div className="question-card">
+          <h2>
+            Question: {currentQuestion + 1} out of {questions.length}
+          </h2>
+          <h3 className="question-text">{questions[currentQuestion].text}</h3>
           <ul>
-            <li>Boston</li>
-            <li>New York City</li>
-            <li>Washington DC</li>
-            <li>Denver</li>
+            {questions[currentQuestion].options.map((option) => {
+              return (
+                <li
+                  key={option.id}
+                  onClick={() => optionClicked(option.isCorrect)}
+                >
+                  {option.text}
+                </li>
+              );
+            })}
           </ul>
         </div>
       )}
